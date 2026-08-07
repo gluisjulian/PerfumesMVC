0cc25f8c8fe9f0068471d55821101e4739135f66
gluisjulian <gluisjulian@gmail.com>
2026-08-07 16:24:05 -0300

    Começo Dashboard

---
+++ IdentityManualApp/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using IdentityManualApp.Data;
+using IdentityManualApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PerfumesMVC.ViewModels.Dashboard;
+
+namespace PerfumesMVC.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        private string? ObterUsuario()
+        {
+            return _userManager.GetUserId(User);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DashboardViewModel model = new DashboardViewModel
+            {
+                Perfume = await _context.Perfumes.Include(p => p.Receita).Where(p => p.UsuarioId == ObterUsuario()).ToListAsync(),
+                QtdePerfumes = _context.Perfumes.Count()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ IdentityManualApp/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PerfumesMVC.Models;
+
+namespace PerfumesMVC.ViewModels.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public int QtdePerfumes { get; set; }
+        public List<PerfumesMVC.Models.Perfume> Perfume { get; set; } = new();
+
+    }
+}
